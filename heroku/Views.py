6b06d1a4c7ff523de3f439e96640b1b1f5ad8b50
Sankar Narayanan <sankar.narayanan@global-analytics.com>
2016-02-08 13:24:52 +0530

    login screen changes..

---
+++ heroku/Views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import HttpResponse
+import json
+from django.shortcuts import render_to_response
+import os
+from django.template import loader
+
+def goToLogin(request):
+    data = {}
+    json_data = {}
+
+    if request.method == "POST":
+        data['request'] = 'request is POST'
+        json_data = json.dumps(data)
+    else:
+        template = loader.get_template('login.html')
+        return HttpResponse(template.render(json_data, request))
+        return render_to_response('login.html')
+        data['request'] = 'request is GET'
+        json_data = json.dumps(data)
+    return HttpResponse(json_data)
